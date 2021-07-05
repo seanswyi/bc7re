a7ac252a65ae229f20f35890d5677bba68ab8c77
seanswyi <seantuokc@gmail.com>
2021-07-05 22:36:06 +0900

    Added AT Loss.

---
+++ at_loss.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ATLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, logits, labels):
+        # TH label
+        labels = labels.reshape(logits.shape)
+        th_label = torch.zeros_like(labels, dtype=torch.float).to(labels)
+        th_label[:, 0] = 1.0
+        labels[:, 0] = 0.0
+
+        p_mask = labels + th_label
+        n_mask = 1 - labels
+
+        # Rank positive classes to TH
+        logit1 = logits - (1 - p_mask) * 1e30
+        loss1 = -(F.log_softmax(logit1, dim=-1) * labels).sum(1)
+
+        # Rank TH to negative classes
+        logit2 = logits - (1 - n_mask) * 1e30
+        loss2 = -(F.log_softmax(logit2, dim=-1) * th_label).sum(1)
+
+        # Sum two parts
+        loss = loss1 + loss2
+        loss = loss.mean()
+
+        return loss
