6adfab4fd7eb00ccf171c6be84c2836d6454a702
seanswyi <seantuokc@gmail.com>
2021-07-05 22:36:25 +0900

    Added in evaluation code.

--- trainer.py
+++ trainer.py
@@ -1,10 +1,18 @@
 import json
 
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm, trange
+from transformers.optimization import get_linear_schedule_with_warmup
+import wandb
 
 from preprocess import convert_data_to_features
+
+
+with open(file='/hdd1/seokwon/data/BC7DP/relation2id.json') as f:
+    relation2id = json.load(fp=f)
+    id2relation = {id_: relation for relation, id_ in relation2id.items()}
 
 
 def collate_fn(batch):
@@ -26,7 +34,7 @@ def collate_fn(batch):
 
 
 class Trainer():
-    def __init__(self, args, config, model, tokenizer):
+    def __init__(self, args, config, model, optimizer, tokenizer):
         self.args = args
         self.config = config
 
@@ -35,15 +43,16 @@ class Trainer():
 
         if args.debug:
             self.train_data = self.train_data[:100]
-            self.dev_data = self.dev_data[:100]
 
         self.batch_size = args.batch_size
         self.num_epochs = args.num_epochs
         self.learning_rate = args.learning_rate
+        self.warmup_ratio = args.warmup_ratio
 
         self.entity_marker = args.entity_marker
 
         self.model = model
+        self.optimizer = optimizer
         self.tokenizer = tokenizer
 
     def load_data(self, filepath):
@@ -56,17 +65,133 @@ class Trainer():
         train_features = convert_data_to_features(data=self.train_data, tokenizer=self.tokenizer, entity_marker=self.entity_marker)
         train_dataloader = DataLoader(train_features, batch_size=self.batch_size, shuffle=True, collate_fn=collate_fn, drop_last=False)
 
+        total_steps = int(len(train_dataloader) * self.num_epochs)
+        warmup_steps = int(total_steps * self.warmup_ratio)
+        scheduler = get_linear_schedule_with_warmup(optimizer=self.optimizer,
+                                                    num_warmup_steps=warmup_steps,
+                                                    num_training_steps=total_steps)
+
+        best_score = 0
+        num_steps = 0
         epoch_pbar = trange(self.num_epochs, desc="Epoch", total=self.num_epochs)
         for epoch in epoch_pbar:
+            self.model.zero_grad()
+
             train_pbar = tqdm(iterable=train_dataloader, desc="Training", total=len(train_dataloader))
             for step, batch in enumerate(train_pbar):
-                inputs = {'input_ids': batch[0],
-                          'attention_mask': batch[1],
+                self.model.train()
+
+                inputs = {'input_ids': batch[0].to('cuda'),
+                          'attention_mask': batch[1].to('cuda'),
                           'entity_positions': batch[2],
                           'head_tail_pairs': batch[3],
                           'labels': batch[4]}
 
                 outputs = self.model(**inputs)
 
-    def evaluate(self,):
-        pass
+                loss = outputs[-1]
+
+                loss.backward()
+                self.optimizer.step()
+                scheduler.step()
+                self.model.zero_grad()
+
+                wandb.log({'loss': loss.item()}, step=num_steps)
+                num_steps += 1
+
+            results = self.evaluate()
+            wandb.log(results, step=num_steps)
+
+            if results['f1'] > best_score:
+                best_score = results['f1']
+                torch.save(self.model.state_dict(), f'/hdd1/seokwon/BC7/checkpoints/atlop_{self.args.model_name_or_path}')
+
+    def evaluate(self, mode='dev'):
+        dev_features = convert_data_to_features(data=self.dev_data, tokenizer=self.tokenizer, entity_marker=self.entity_marker, mode='dev')
+        dev_dataloader = DataLoader(dev_features, batch_size=self.batch_size, shuffle=False, collate_fn=collate_fn, drop_last=False)
+
+        preds = []
+        dev_pbar = tqdm(iterable=dev_dataloader, desc=f"Evaluating {mode}", total=len(dev_dataloader))
+        for batch in dev_pbar:
+            self.model.eval()
+
+            inputs = {'input_ids': batch[0].to('cuda'),
+                      'attention_mask': batch[1].to('cuda'),
+                      'entity_positions': batch[2],
+                      'head_tail_pairs': batch[3]}
+
+            with torch.no_grad():
+                output = self.model(**inputs)
+                pred = output[0]
+                pred = pred.cpu().numpy()
+                preds.append(pred)
+
+        predictions = np.concatenate(preds, axis=0).astype(np.float32)
+        answers = self.convert_to_evaluation_features(predictions, dev_features)
+
+        if answers == []:
+            precision = 0
+            recall = 0
+            f1 = 0
+        else:
+            tp = 0
+
+            true_converted = []
+            for document in self.dev_data:
+                pmid = document['doc_id']
+                relations = document['relations']
+
+                for relation in relations:
+                    template = {'pmid': pmid, 'head_idx': relation['head_idx'], 'tail_idx': relation['tail_idx'], 'relation': relation['relation']}
+                    true_converted.append(template)
+
+            positive_predictions = [x for x in answers if x['relation'] != 'Na']
+            tp_plus_fp = len(positive_predictions)
+            tp_plus_fn = len(true_converted)
+
+            for prediction in positive_predictions:
+                if pred in true_converted:
+                    tp += 1
+
+            if tp_plus_fp == 0:
+                precision = 0
+            else:
+                precision = tp / tp_plus_fp
+
+            if tp_plus_fn == 0:
+                recall = 0
+            else:
+                recall = tp / tp_plus_fn
+
+            if precision + recall == 0:
+                f1 = 0
+            else:
+                f1 = 2 * (precision * recall) / (precision + recall)
+
+        return {'precision': precision, 'recall': recall, 'f1': f1}
+
+    def convert_to_evaluation_features(self, predictions, features):
+        head_idxs = []
+        tail_idxs = []
+        doc_ids = []
+
+        for feature in features:
+            head_tail_pairs = feature['head_tail_pairs']
+            head_idxs += [('T' + str(pair[0] + 1)) for pair in head_tail_pairs]
+            tail_idxs += [('T' + str(pair[1] + 1)) for pair in head_tail_pairs]
+            doc_ids += [feature['doc_id'] for pair in head_tail_pairs]
+
+        results = []
+        for idx, _ in enumerate(predictions):
+            prediction = predictions[idx]
+            prediction = np.nonzero(prediction)[0].tolist()
+
+            for pred in prediction:
+                if pred != 0:
+                    result_dict = {'pmid': doc_ids[idx],
+                                   'head_idx': head_idxs[idx],
+                                   'tail_idx': tail_idxs[idx],
+                                   'relation': id2relation[pred]}
+                    results.append(result_dict)
+
+        return results
