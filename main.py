3946a4ae475f3a30a661485123e1d23ba591a022
seanswyi <seantuokc@gmail.com>
2021-07-05 22:36:13 +0900

    Added in optimizer.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
 import os
 
+import torch.optim as optim
 from transformers import AutoConfig, AutoModel, AutoTokenizer
+import wandb
 
 from model import DrugProtREModel
 from trainer import Trainer
@@ -16,14 +18,24 @@ def main(args):
     backbone_model = AutoModel.from_pretrained(args.model_name_or_path)
 
     model = DrugProtREModel(args=args, config=config, backbone_model=backbone_model, tokenizer=tokenizer)
+    model = model.to('cuda')
 
-    trainer = Trainer(args=args, config=config, model=model, tokenizer=tokenizer)
+    new_layer = ['extractor', 'bilinear']
+    optimizer_grouped_parameters = [
+        {"params": [p for n, p in model.named_parameters() if not any(nd in n for nd in new_layer)], },
+        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in new_layer)], 'lr': 1e-4},
+    ]
+    optimizer = optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+
+    trainer = Trainer(args=args, config=config, model=model, optimizer=optimizer, tokenizer=tokenizer)
     trainer.train()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--adam_epsilon', default=1e-6, type=float)
+    parser.add_argument('--adaptive_thresholding_k', default=1, type=int)
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--bilinear_block_size', default=64, type=int)
     parser.add_argument('--data_dir', default='/hdd1/seokwon/data/BC7DP/drugprot-gs-training-development', type=str)
@@ -34,9 +46,12 @@ if __name__ == '__main__':
     parser.add_argument('--max_seq_len', default=512, type=str)
     parser.add_argument('--model_name_or_path', default='dmis-lab/biobert-v1.1', type=str)
     parser.add_argument('--num_epochs', default=10, type=int)
-    parser.add_argument('--num_labels', default=13, type=int)
+    parser.add_argument('--num_labels', default=14, type=int)
     parser.add_argument('--train_file', default='train.json', type=str)
+    parser.add_argument('--warmup_ratio', default=0.06, type=float)
 
     args = parser.parse_args()
+
+    wandb.init(project='BC7DP', name="ATLOP BioBERT")
 
     main(args)
