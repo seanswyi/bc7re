021a7c310473a37ce4b69e57cc93adde9a4b232c
seanswyi <seantuokc@gmail.com>
2021-07-07 08:23:29 +0900

    Added in new arguments and changed W&B naming.

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ def main(args):
     model = DrugProtREModel(args=args, config=config, backbone_model=backbone_model, tokenizer=tokenizer)
     model = model.to('cuda')
 
-    new_layer = ['extractor', 'bilinear']
+    new_layer = ['extractor', 'classifier']
     optimizer_grouped_parameters = [
         {"params": [p for n, p in model.named_parameters() if not any(nd in n for nd in new_layer)], },
         {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in new_layer)], 'lr': 1e-4},
     ]
     optimizer = optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+    # optimizer = optim.Adam(params=model.parameters(), lr=args.learning_rate)
 
     trainer = Trainer(args=args, config=config, model=model, optimizer=optimizer, tokenizer=tokenizer)
     trainer.train()
@@ -38,6 +39,8 @@ if __name__ == '__main__':
     parser.add_argument('--adaptive_thresholding_k', default=1, type=int)
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--bilinear_block_size', default=64, type=int)
+    parser.add_argument('--classification_type', default='entity_marker', choices=['cls', 'entity_marker'])
+    parser.add_argument('--classifier_type', default='bilinear', choices=['linear', 'bilinear'])
     parser.add_argument('--data_dir', default='/hdd1/seokwon/data/BC7DP/drugprot-gs-training-development', type=str)
     parser.add_argument('--debug', action='store_true', default=False)
     parser.add_argument('--dev_file', default='dev.json', type=str)
@@ -45,14 +48,23 @@ if __name__ == '__main__':
     parser.add_argument('--learning_rate', default=5e-5, type=float)
     parser.add_argument('--max_seq_len', default=512, type=str)
     parser.add_argument('--model_name_or_path', default='dmis-lab/biobert-v1.1', type=str)
-    parser.add_argument('--negative_ratio', default=2, type=int)
+    parser.add_argument('--negative_ratio', default=2, type=float)
     parser.add_argument('--num_epochs', default=10, type=int)
     parser.add_argument('--num_labels', default=14, type=int)
     parser.add_argument('--train_file', default='train.json', type=str)
+    parser.add_argument('--use_at_loss', action='store_true', default=False)
     parser.add_argument('--warmup_ratio', default=0.06, type=float)
 
     args = parser.parse_args()
 
-    wandb.init(project='BC7DP', name="ATLOP BioBERT")
+    if 'biobert' in args.model_name_or_path:
+        model_name = 'biobert'
+
+    wandb_name = f'{model_name}_{args.classification_type}_{args.classifier_type}_{args.learning_rate}_{args.negative_ratio}'
+
+    if args.use_at_loss:
+        wandb_name = 'AT_' + wandb_name
+
+    wandb.init(project='BC7DP', name=wandb_name)
 
     main(args)
