ea80661037dfb0aaa8645bcacc570b5002289a74
seanswyi <seantuokc@gmail.com>
2021-07-19 10:29:59 +0900

    Added in logging and new arguments.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import argparse
+from datetime import datetime
+import logging
 import os
+import time
 
 import torch.optim as optim
 from transformers import AutoConfig, AutoModel, AutoTokenizer
@@ -9,9 +12,17 @@ from model import DrugProtREModel
 from trainer import Trainer
 
 
+logger = logging.getLogger()
+
+
 def main(args):
     args.train_file = os.path.join(args.data_dir, 'training', args.train_file)
     args.dev_file = os.path.join(args.data_dir, 'development', args.dev_file)
+    args.entity_file = os.path.join(args.data_dir, 'development', args.entity_file)
+
+    logger.info('Arguments:')
+    for name, value in vars(args).items():
+        logger.info(f'\t{name}: {value}')
 
     config = AutoConfig.from_pretrained(args.model_name_or_path)
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
@@ -50,9 +61,12 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir', default='/hdd1/seokwon/data/BC7DP/drugprot-gs-training-development', type=str)
     parser.add_argument('--debug', action='store_true', default=False)
     parser.add_argument('--dev_file', default='dev.json', type=str)
+    parser.add_argument('--dont_save', action='store_true', default=False)
     parser.add_argument('--entity_marker', default='asterisk', type=str, choices=['asterisk', 'typed'])
+    parser.add_argument('--entity_file', default='drugprot_development_entities.tsv')
     parser.add_argument('--evaluation_step', default=250, type=int)
     parser.add_argument('--learning_rate', default=5e-5, type=float)
+    parser.add_argument('--log_file_dir', default='/hdd1/seokwon/BC7/logs', type=str)
     parser.add_argument('--max_seq_len', default=512, type=str)
     parser.add_argument('--model_name_or_path', default='dmis-lab/biobert-v1.1', type=str, \
         choices=['dmis-lab/biobert-v1.1', '/hdd1/seokwon/BC7/checkpoints/RoBERTa-base-PM-M3-Voc-distill/'])
@@ -67,12 +81,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    time_right_now = time.time()
+    timestamp = datetime.fromtimestamp(timestamp=time_right_now).strftime(format='%m-%d-%Y-%H%M')
+    args.timestamp = timestamp
+
     if 'biobert' in args.model_name_or_path:
         model_name = 'biobert'
     elif 'roberta' in args.model_name_or_path.lower():
         model_name = 'biolm'
 
-    wandb_name = f'{model_name}_{args.classification_type}_{args.classifier_type}_{args.learning_rate}_{args.classifier_learning_rate}_{args.negative_ratio}'
+    wandb_name = f'{model_name}_{args.classification_type}_{args.classifier_type}_{args.learning_rate}_{args.classifier_learning_rate}_{args.negative_ratio}_{args.timestamp}'
 
     if args.use_at_loss:
         wandb_name = 'AT_' + wandb_name
@@ -83,8 +101,16 @@ if __name__ == '__main__':
     args.wandb_name = wandb_name
     wandb.init(project='BC7DP', name=wandb_name)
 
-    print('Arguments:')
-    for name, value in vars(args).items():
-        print(f'\t{name}: {value}')
+    if not os.path.exists(args.log_file_dir):
+        os.makedirs(args.log_file_dir, exist_ok=True)
+
+    args.log_filename = os.path.join(args.log_file_dir, f'{wandb_name}.log')
+
+    log_msg_format = '[%(asctime)s - %(levelname)s - %(filename)s: %(lineno)d] %(message)s'
+    logging.basicConfig(format=log_msg_format,
+                        level=logging.INFO,
+                        handlers=[logging.FileHandler(filename=args.log_filename), logging.StreamHandler()])
+
+    logger.info("Starting main process...")
 
     main(args)
